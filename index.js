48a83d4ef7fd538aaabce958c25362a567a1d33e
SevenTrio <36824327+SevenTrio@users.noreply.github.com>
2019-02-18 19:03:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+const client = new Discord.Client();
+client.config = require("./config.js");
+
+require("./modules/functions.js")(client);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+    props.conf.aliases.forEach(alias => {
+    client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.levelCache = {};
+for (let i = 0; i < client.config.permLevels.length; i++) {
+  const thisLevel = client.config.permLevels[i];
+  client.levelCache[thisLevel.name] = thisLevel.level;
+}
+
+client.login(client.config.token);
